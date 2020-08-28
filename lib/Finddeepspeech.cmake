69d258a213488aec880bebb19208cc8eafa678f9
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-28 15:28:56 -0700

    corrected build on linux

--- lib/Finddeepspeech.cmake
+++ lib/Finddeepspeech.cmake
@@ -2,6 +2,11 @@ add_library(deepspeech INTERFACE IMPORTED)
 if(WIN32)
 	target_link_libraries(deepspeech PUBLIC INTERFACE "${CMAKE_CURRENT_LIST_DIR}/deepspeech/amd64.cpu.win/libdeepspeech.so.if.lib")
 	set(DEEPSPEECH_SHARED_LIBRARY_DIR "amd64.cpu.win")
+	configure_file(
+		"${CMAKE_CURRENT_LIST_DIR}/deepspeech/${DEEPSPEECH_SHARED_LIBRARY_DIR}/libdeepspeech.so"
+		"${CMAKE_CURRENT_BINARY_DIR}/libdeepspeech.so"
+		COPYONLY
+	)
 elseif(APPLE)
 	set(DEEPSPEECH_SHARED_LIBRARY_DIR "amd64.cpu.osx")
 elseif(UNIX)
@@ -9,9 +14,9 @@ elseif(UNIX)
 else()
 	message(FATAL_ERROR "No deepspeech library configured for this platform")
 endif()
-configure_file(
-	"${CMAKE_CURRENT_LIST_DIR}/deepspeech/${DEEPSPEECH_SHARED_LIBRARY_DIR}/libdeepspeech.so"
-	"${CMAKE_CURRENT_BINARY_DIR}/libdeepspeech.so"
-	COPYONLY
-)
+if(NOT WIN32)
+	target_link_libraries(deepspeech PUBLIC INTERFACE "${CMAKE_CURRENT_LIST_DIR}/deepspeech/${DEEPSPEECH_SHARED_LIBRARY_DIR}/libdeepspeech.so")
+endif()
 target_include_directories(deepspeech PUBLIC INTERFACE "${CMAKE_CURRENT_LIST_DIR}/deepspeech/")
+
+mark_as_advanced(DEEPSPEECH_SHARED_LIBRARY_DIR)
