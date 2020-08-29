4bb1e45ed15a56646f83ee96ba4dfaaf4a8d88d8
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-28 21:07:14 -0700

    replace portaudio and asio with audiofile

---
+++ lib/Findaudiofile.cmake
@@ -0,0 +1,2 @@
+add_library(audiofile INTERFACE)
+target_include_directories(audiofile PUBLIC INTERFACE "${CMAKE_CURRENT_LIST_DIR}/audiofile")
--- src/speech.cpp
+++ src/speech.cpp
@@ -3,22 +3,12 @@
 #include <filesystem>
 
 #include "deepspeech.h"
-#include "portaudio.h"
+#include "audiofile.h"
 
 #include "model.h"
 #include "deepsmartptrs.h"
 
-namespace fs = std::filesystem;
-
 int main() {
-    PaError paErr = Pa_Initialize();
-    if (paErr != paNoError) {
-        std::cerr << "Portaudio initialize error: " << Pa_GetErrorText(paErr) << '\n';
-        return 1;
-    }
-    std::cout << "Initialized portaudio" << '\n';
-
-    std::cout << MODEL_PATH << '\n';
     ModelState *rawCtx;
     int status = DS_CreateModel(MODEL_PATH, &rawCtx);
     if (status != 0) {
@@ -26,14 +16,10 @@ int main() {
         std::cerr << "Couldn't create model: " << err.get() << '\n';
         return 1;
     }
-    std::cout << "Model created!" << '\n';
+    std::cout << "Model created" << '\n';
     UniqueModelState ctx(rawCtx);
 
 
-    paErr = Pa_Terminate();
-    if (paErr != paNoError) {
-        std::cerr << "Portaudio termination error: " << Pa_GetErrorText(paErr) << '\n';
-        return 1;
-    }
+
     return 0;
 }
